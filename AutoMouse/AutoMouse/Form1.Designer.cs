9be0aa41d207752bffb917fc39870228d41355f8
Jessepx <Jessepx@gmail.com>
2018-06-13 15:22:37 -0700

    work PC upload
    Click functionality
    Cursor relocation
    - Will need to implement a delay/timing with click

--- AutoMouse/AutoMouse/Form1.Designer.cs
+++ AutoMouse/AutoMouse/Form1.Designer.cs
@@ -31,7 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -49,15 +51,26 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(35, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(130, 17);
+            this.label1.Size = new System.Drawing.Size(96, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Cursor Coordinates";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(189, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clicker";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(300, 100);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -70,8 +83,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- AutoMouse/AutoMouse/Form1.cs
+++ AutoMouse/AutoMouse/Form1.cs
@@ -12,21 +12,30 @@ namespace AutoMouse
 {
     public partial class Form1 : Form
     {
-        Point clickLocation;
+        Point clickLocation = new Point();
         bool locOn = false;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, Point loc);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        //button1 should store the location of the Cursor into a Point object, allowing button2 to access that stored Point.
         private void button1_Click(object sender, EventArgs e)
         {
 
             if (locOn == false)
             {
                 timer1.Tick += new EventHandler(button1_Click);
-                timer1.Interval = 2000;
+                timer1.Interval = 1000;
                 timer1.Start();
                 label1.Text = "Getting Location..";
                 locOn = true;
@@ -40,9 +49,35 @@ namespace AutoMouse
             }
         }
 
+        //label1 displays the X,Y coordinates of the stored location
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
+
+        //Uses the imported user32.dll to implement "Clicking" functionality when button2 is pressed
+        public void MouseClick()
+        {
+            int count = 0;
+            timer2.Tick += new EventHandler(button2_Click);
+            timer2.Interval = 2000;
+            timer2.Start();
+            Cursor.Position = clickLocation;
+            while (count < 5)
+            {
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, clickLocation);
+                count++;
+            }
+            timer2.Stop();
+        }
+
+        //Where we are calling the "Clicking" functionality to occur
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MouseClick();
+        }
+
+        
+
     }
 }
