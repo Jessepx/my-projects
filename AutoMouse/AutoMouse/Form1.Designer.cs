09f583668ebbd0fc4caf5908564b59b52c62a3b0
Jesse Mangano <Jessepx@gmail.com>
2018-06-20 20:54:25 -0700

    asdf

--- AutoMouse/AutoMouse/Form1.Designer.cs
+++ AutoMouse/AutoMouse/Form1.Designer.cs
@@ -34,7 +34,6 @@
             this.timer2 = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
-            this.timer3 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
@@ -67,6 +66,7 @@
             this.button2.Text = "Clicker";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+
             // 
             // Form1
             // 
@@ -74,13 +74,12 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "AutoMouse";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
+            this.KeyPreview = true;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(Form1_KeyDown);
 
         }
 
@@ -91,7 +90,6 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Timer timer3;
     }
 }
 
--- AutoMouse/AutoMouse/Form1.cs
+++ AutoMouse/AutoMouse/Form1.cs
@@ -5,10 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Diagnostics;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
-using Hotkeys;
 
 namespace AutoMouse
 {
@@ -16,9 +14,9 @@ namespace AutoMouse
     {
         Point clickLocation = new Point();
         bool locOn = false;
+        bool forceQuit = false;
         bool buttonCheck = false;
         bool clickCheck = false;
-        private Hotkeys.GlobalHotkey ghk;
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
         public static extern void mouse_event(int dwFlags, Point loc);
@@ -31,44 +29,6 @@ namespace AutoMouse
         public Form1()
         {
             InitializeComponent();
-            ghk = new Hotkeys.GlobalHotkey(Constants.SHIFT, Keys.Q, this);
-        }
-
-        private void HandleHotkey()
-        {
-            timer2.Stop();
-            timer2.Dispose();
-            timer3.Stop();
-            timer3.Dispose();
-            label1.Text = "Clicking Stopped.\nReset Coordinates.";
-        }
-
-        protected override void WndProc(ref Message m)
-        {
-            if (m.Msg == Hotkeys.Constants.WM_HOTKEY_MSG_ID)
-                HandleHotkey();
-            base.WndProc(ref m);
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            WriteLine("Trying to register SHIFT+Q");
-            if (ghk.Register())
-                WriteLine("Hotkey SHIFT+Q registered.");
-            else
-                WriteLine("Hotkey failed to register");
-        }
-
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            if (!ghk.Unregister())
-                MessageBox.Show("Hotkey failed to unregister!");
-        }
-
-        private void WriteLine(string text)
-        {
-            //label1.Text = text + Environment.NewLine;
-
         }
 
         //button1 should store the location of the Cursor into a Point object, allowing button2 to access that stored Point.
@@ -86,7 +46,6 @@ namespace AutoMouse
             else
             {
                 timer1.Stop();
-                timer1.Dispose();
                 clickLocation = Cursor.Position;
                 label1.Text = "Location set:\n" + clickLocation.ToString();
                 locOn = false;
@@ -105,8 +64,7 @@ namespace AutoMouse
         //Uses the imported user32.dll to implement "Clicking" functionality when button2 is pressed
         private void button2_Click(object sender, EventArgs e)
         {
-            timer2.Tick += new EventHandler(button2_Click);
-            
+            int count = 0;
 
             if (buttonCheck == false)
             {
@@ -116,28 +74,32 @@ namespace AutoMouse
             {
                 if (clickCheck == false)
                 {
-                    clickCheck = true;
+                    timer2.Tick += new EventHandler(button2_Click);
                     timer2.Interval = 2000;
                     timer2.Start();
+                    clickCheck = true;
                 }
                 else
                 {
-                    clickCheck = false;
-                    timer2.Stop();
-                    timer2.Dispose();
                     Cursor.Position = clickLocation; //Moves cursor to stored location
-                    mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, clickLocation);
-                    timer3.Tick += new EventHandler(ClickMouse);
-                    timer3.Interval = 2000;
-                    timer3.Start();
+                    while (count < 1) //While loop that clicks ONCE now, for testing
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, clickLocation);
+                        count++;
+                    }
+                    timer2.Stop();
+                    clickCheck = false;
                 }
             }
         }
 
-        private void ClickMouse(object sender, EventArgs e)
+        private void Form1_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
         {
-            Cursor.Position = clickLocation; //Moves cursor to stored location
-            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, clickLocation);
+            // Determine whether the keystroke is a number from the top of the keyboard.
+            if (e.KeyCode == Keys.Q)
+            {
+                Application.Exit();
+            }
         }
     }
 }
